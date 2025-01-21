96dff6157612c5ceaaa596841e858cd50b7d6e26
Yan Yang <yang.yan@rocketmail.com>
2025-01-21 11:06:29 +1000

    add flow matching

---
+++ modules/flow_matching.py
@@ -0,0 +1,23 @@
+import torch
+
+
+class FlowMatching:
+    def __init__(self, sigma=0.1, device="cpu"):
+        self.sigma = sigma
+        self.device = device
+
+    def compute_flow(self, x0, x1, t):
+        """
+        flow interpolation at time t
+        """
+        return t*x1 + (1-t)*x0
+    
+    def sample_xt(self, x0, x1, t):
+        """
+        sample from the distribution p(x_t|x_0, x_1)
+        """
+        flow = self.compute_flow(x0, x1, t)
+        noise = torch.randn_like(x0).to(self.device)
+        xt = flow  + self.sigma * noise
+        conditional_flow = x1 - x0
+        return xt, conditional_flow
