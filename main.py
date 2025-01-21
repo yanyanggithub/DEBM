787c608e2221fcd413968fed213fcb7080e9c5ef
Yan Yang <yang.yan@rocketmail.com>
2025-01-21 12:05:16 +1000

    added utils

--- main.py
+++ main.py
@@ -8,17 +8,14 @@ python3 main.py --model_name=rbm --dataset_name=mnist --n_epochs=100
 """
 
 import os.path
-import matplotlib.pyplot as plt
 import torch
-from torchvision import datasets
-from torchvision import transforms
 from modules.stacked_rbm import StackedRBM
 from modules.diffusion import Diffusion
 from modules.unet import Unet
-from tqdm import tqdm
-import numpy as np
 from ast import literal_eval
 import sys
+
+from utils import plot, load_dataset, Trainer
 
 dataset_name = 'mnist'  # 'mnist' or 'cifar10'
 model_name = 'rbm' # 'rbm' or 'diffusion'
@@ -37,110 +34,6 @@ if torch.cuda.is_available():
     torch.cuda.set_device("cuda:0")
 
 
-def plot(X, img_shape, filename):
-    X = X.to("cpu")
-    X = X.detach().numpy()
-    fig = plt.figure(figsize=(5, 5))
-    for i in range(25): 
-        sub = fig.add_subplot(5, 5, i+1)
-        img = X[i, :].reshape(img_shape)
-        if len(img_shape) == 3:
-            img = np.moveaxis(img, 0, -1)
-        sub.imshow(img, cmap=plt.cm.gray)
-    
-    plt.setp(plt.gcf().get_axes(), xticks=[], yticks=[])
-    plt.tight_layout()
-    plt.savefig(filename)
-
-
-def train_rmb(model, train_loader, 
-              checkpt="./output/chk_rbm.pt", n_epochs=10, 
-              lr=0.01, batch_size=64):
-    checkpt_epoch = 0
-    if os.path.isfile(checkpt):
-        checkpoint = torch.load(checkpt, weights_only=True)
-        model.load_state_dict(checkpoint['state_dict'])
-        checkpt_epoch = checkpoint['epoch']
-        lr = checkpoint['lr']
-    model.train()
-
-    for epoch in range(n_epochs):
-        loss_ = []
-        if epoch and epoch % 10 == 0:
-            # learning rate decay
-            lr = lr * 0.9
-        for _, (data, _) in enumerate(train_loader):
-            data = data.to(device)
-            input = data.view(-1, model.n_visible)
-            loss = model.fit(input, lr=lr, batch_size=batch_size)
-            loss_.append(loss.item())
-
-        epoch_ = epoch + checkpt_epoch + 1
-        checkpoint = {
-            'epoch': epoch_,
-            'state_dict': model.state_dict(),
-            'lr': lr
-        }
-
-        torch.save(checkpoint, checkpt)
-        print('Epoch %d Loss=%.4f' % (epoch_, np.mean(loss_)))
-
-    return model
-
-
-def train_diffusion(model, train_loader, 
-                   checkpt="./output/chk_diffusion.pt", n_epochs=10, 
-                   lr=0.01):
-    checkpt_epoch = 0
-    if os.path.isfile(checkpt):
-        checkpoint = torch.load(checkpt, weights_only=True)
-        model.load_state_dict(checkpoint['state_dict'])
-        checkpt_epoch = checkpoint['epoch']
-        lr = checkpoint['lr']
-    model.train()
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    loss_fn = torch.nn.MSELoss()
-    diffusion = Diffusion(timesteps=1000, device=device)
-
-    for epoch in range(n_epochs):
-        loss_ = []
-        for _, (data, _) in enumerate(tqdm(train_loader)):
-            data = data.to(device)
-            if dataset_name == 'mnist':
-                # [batch_size, 1, 28, 28]
-                data.unsqueeze(3)
-                data = data * 2 - 1
-            t = torch.randint(0, diffusion.timesteps, (data.shape[0],))
-            t = t.to(device)
-            noise = torch.randn_like(data).to(device)
-            noisy_data = diffusion.add_noise(data, noise, t)
-            optimizer.zero_grad()
-            pred_noise = model(noisy_data, t)
-            loss = loss_fn(pred_noise, noise)
-            loss_.append(loss.item())
-            loss.backward()
-            optimizer.step()
-
-        epoch_ = epoch + checkpt_epoch + 1
-        checkpoint = {
-            'epoch': epoch_,
-            'state_dict': model.state_dict(),
-            'lr': lr
-        }
-
-        torch.save(checkpoint, checkpt)
-        print('Epoch %d Loss=%.4f' % (epoch_, np.mean(loss_)))
-
-    return model
-
-
-def stack_samples(gen_samples, stack_dim):
-    gen_samples = list(torch.split(gen_samples, 1, dim=1))
-    for i in range(len(gen_samples)):
-        gen_samples[i] = gen_samples[i].squeeze(1)
-    return torch.cat(gen_samples, dim=stack_dim)
-
-
 def main_diffusion(train_dataset, checkpt_file, img_shape):
     if dataset_name == 'mnist':
         n_channels = 1
@@ -148,16 +41,13 @@ def main_diffusion(train_dataset, checkpt_file, img_shape):
         n_channels = 3
 
     model = Unet(n_channels, t_emb_dim=128, device=device)
-    model.to(device)
 
     train_loader = torch.utils.data.DataLoader(train_dataset, 
                                             batch_size=batch_size, 
                                             shuffle=True)
-    model = train_diffusion(model, train_loader, 
-                            checkpt=checkpt_file,
-                            n_epochs=n_epochs, 
-                            lr=learning_rate)
-
+    trainer = Trainer(model, dataset_name, checkpt=checkpt_file, n_epochs=n_epochs, 
+                      lr=learning_rate, batch_size=batch_size, device=device)
+    model = trainer.train_diffusion(train_loader)
     sample_batch_size = 25
 
     # image gen from noise
@@ -176,7 +66,6 @@ def main_diffusion(train_dataset, checkpt_file, img_shape):
 
 def main_rbm(train_dataset, checkpt_file, img_shape):
     k = 1
-
     if dataset_name == 'mnist':
         filter1_shape = (16, 16)
         n_nodes = [784, 256, 128]
@@ -189,12 +78,12 @@ def main_rbm(train_dataset, checkpt_file, img_shape):
                                                shuffle=True)
 
     model = StackedRBM(n_nodes, k)
-    model.to(device)
 
-    model = train_rmb(model, train_loader, 
-                      checkpt=checkpt_file,
-                      n_epochs=n_epochs, 
-                      lr=learning_rate, batch_size=batch_size)
+    trainer = Trainer(model, dataset_name, checkpt=checkpt_file, n_epochs=n_epochs, 
+                      lr=learning_rate, batch_size=batch_size, device=device)
+    
+    model = trainer.train_rbm(train_loader)
+
     w0 = model.rbm_modules[0].weight
     w1 = model.rbm_modules[1].weight
 
@@ -235,19 +124,7 @@ if __name__ == "__main__":
     checkpt = 'chk_' + model_name + '_' + dataset_name + '.pt'
     checkpt_file = os.path.join(output_dir, checkpt)
         
-    if dataset_name == 'mnist':
-        transform = transforms.Compose([transforms.ToTensor()])  
-        train_dataset = datasets.MNIST('./data', train=True, download=True, 
-                                        transform=transform)
-        img_shape = (28, 28)
-    elif dataset_name == 'cifar10':
-        transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), 
-                                 (0.5, 0.5, 0.5))])
-        train_dataset = datasets.CIFAR10('./data', train=True, download=True, 
-                                        transform=transform)  
-        img_shape = (3, 32, 32)
+    train_dataset, img_shape = load_dataset(dataset_name)
 
     if model_name == 'rbm':
         main_rbm(train_dataset, checkpt_file, img_shape)
---
+++ utils.py
@@ -0,0 +1,125 @@
+import matplotlib.pyplot as plt
+import torch
+from torchvision import datasets
+from torchvision import transforms
+import numpy as np
+from tqdm import tqdm
+import numpy as np
+from modules.diffusion import Diffusion
+import os
+
+
+def plot(X, img_shape, filename):
+    X = X.to("cpu")
+    X = X.detach().numpy()
+    fig = plt.figure(figsize=(5, 5))
+    for i in range(25): 
+        sub = fig.add_subplot(5, 5, i+1)
+        img = X[i, :].reshape(img_shape)
+        if len(img_shape) == 3:
+            img = np.moveaxis(img, 0, -1)
+        sub.imshow(img, cmap=plt.cm.gray)
+    
+    plt.setp(plt.gcf().get_axes(), xticks=[], yticks=[])
+    plt.tight_layout()
+    plt.savefig(filename)
+
+
+def load_dataset(dataset_name):
+    if dataset_name == 'mnist':
+        transform = transforms.Compose([transforms.ToTensor()])  
+        train_dataset = datasets.MNIST('./data', train=True, download=True, 
+                                        transform=transform)
+        img_shape = (28, 28)
+    elif dataset_name == 'cifar10':
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), 
+                                 (0.5, 0.5, 0.5))])
+        train_dataset = datasets.CIFAR10('./data', train=True, download=True, 
+                                        transform=transform)  
+        img_shape = (3, 32, 32)
+    
+    return train_dataset, img_shape
+
+class Trainer:
+    def __init__(self, model, dataset_name='mnist',
+                  checkpt="./output/chk_rbm.pt", n_epochs=10, 
+                  lr=0.01, batch_size=64, device="cpu"): 
+        self.model = model
+        self.dataset_name = dataset_name
+        self.device = device
+        self.checkpt = checkpt
+        self.n_epochs = n_epochs
+        self.lr = lr
+        self.batch_size = batch_size
+        self.device = device
+        self.checkpt_epoch = 0
+        self.setup()
+    
+    def setup(self):
+        if torch.cuda.is_available():
+            self.device = "cuda"
+            torch.cuda.set_device("cuda:0")
+        if os.path.isfile(self.checkpt):
+            checkpoint = torch.load(self.checkpt, weights_only=True)
+            self.model.load_state_dict(checkpoint['state_dict'])
+            self.checkpt_epoch = checkpoint['epoch']
+            self.lr = checkpoint['lr']
+        self.model.to(self.device)
+        self.model.train()
+
+    def save_chekpoint(self, epoch_):
+        checkpoint = {
+            'epoch': epoch_,
+            'state_dict': self.model.state_dict(),
+            'lr': self.lr
+        }
+        torch.save(checkpoint, self.checkpt)
+
+    def train_rbm(self, train_loader):
+        for epoch in range(self.n_epochs):
+            loss_ = []
+            if epoch and epoch % 10 == 0:
+                # learning rate decay
+                lr = lr * 0.9
+            for _, (data, _) in enumerate(train_loader):
+                data = data.to(self.device)
+                input = data.view(-1, self.model.n_visible)
+                loss = self.model.fit(input, self.lr, self.batch_size)
+                loss_.append(loss.item())
+
+            epoch_ = epoch + self.checkpt_epoch + 1
+            self.save_chekpoint(epoch_) 
+
+            print('Epoch %d Loss=%.4f' % (epoch_, np.mean(loss_)))
+        return self.model
+
+    def train_diffusion(self, train_loader):    
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+        loss_fn = torch.nn.MSELoss()
+        diffusion = Diffusion(timesteps=1000, device=self.device)
+
+        for epoch in range(self.n_epochs):
+            loss_ = []
+            for _, (data, _) in enumerate(tqdm(train_loader)):
+                data = data.to(self.device)
+                if self.dataset_name == 'mnist':
+                    # [batch_size, 1, 28, 28]
+                    data.unsqueeze(3)
+                    data = data * 2 - 1
+                t = torch.randint(0, diffusion.timesteps, (data.shape[0],))
+                t = t.to(self.device)
+                noise = torch.randn_like(data).to(self.device)
+                noisy_data = diffusion.add_noise(data, noise, t)
+                optimizer.zero_grad()
+                pred_noise = self.model(noisy_data, t)
+                loss = loss_fn(pred_noise, noise)
+                loss_.append(loss.item())
+                loss.backward()
+                optimizer.step()
+
+            epoch_ = epoch + self.checkpt_epoch + 1
+            self.save_chekpoint(epoch_) 
+            print('Epoch %d Loss=%.4f' % (epoch_, np.mean(loss_)))
+        return self.model
