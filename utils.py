66dac94d1dabcbbf25c2b913db0727bf63805739
Yan Yang <yang.yan@rocketmail.com>
2025-02-05 08:49:33 +1000

    cifar10 normalisation

--- utils.py
+++ utils.py
@@ -36,8 +36,8 @@ def load_dataset(dataset_name):
     elif dataset_name == 'cifar10':
         transform = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), 
-                                 (0.5, 0.5, 0.5))])
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], 
+                                 std=[0.229, 0.224, 0.225])])
         train_dataset = datasets.CIFAR10('./data', train=True, download=True, 
                                         transform=transform)  
         img_shape = (3, 32, 32)
