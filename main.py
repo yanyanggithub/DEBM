73f33dcfc1ac87a4e08115efc58a1e00cdbd218f
Yan Yang <yang.yan@rocketmail.com>
2025-02-05 11:46:11 +1000

    variable update

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ def parse_args():
 if __name__ == "__main__":
     parse_args()
     experiment_name = model_name + '_' + dataset_name
-    checkpt = 'chk_' + model_name + '_' + dataset_name + '.pt'
+    checkpt = 'chk_' + experiment_name + '.pt'
     checkpt_file = os.path.join(output_dir, checkpt)        
     train_dataset, img_shape = load_dataset(dataset_name)
 
